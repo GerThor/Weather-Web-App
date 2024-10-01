a7628b60326785cf3fe83d96e15d61897b5b7342
Jack Thor <jack.sen.thor@gmail.com>
2024-09-30 21:15:28 -0700

    initial set up for Flask
    Updated .gitignore file to ignore python packages and artifacts. Updated requirements.txt to contain dependency and bat file to install dependency

---
+++ setup.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM Check if a virtual environment directory exists, if not, create it
+if not exist "venv" (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+
+REM Activate the virtual environment
+echo Activating virtual environment...
+call venv\Scripts\activate
+
+REM Install packages from requirements.txt
+echo Installing packages from requirements.txt...
+pip install -r requirements.txt
+
+REM Update requirements.txt with current environment packages
+echo Updating requirements.txt with current packages...
+pip freeze > requirements.txt
+
+echo Setup complete!
