c70f613c0a3c22cccd10391df0cc0bc71ab04439
Ger Thor <ger.thor97@gmail.com>
2024-10-03 17:02:57 -0700

    Added More API Calling Code
    Able to obtain specific US city and state longitude and latitude coordinates. However, am failing to make an API call to obtain current weather data. Failing with Error 401, representing unauthorized permissions error

--- main.py
+++ main.py
@@ -49,23 +49,50 @@ def get_data():
     # that will obtain more fields such as temperature and etc.
 
 
+    # Must use q=city,state_code,country_code or else you will get an object containing too many different Dallas location data and it will be messy to extract the correct coordinates
+    # state_code = 'TX'
+    # country_code = 'US'
+    
 
-    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q=[{city}]&limit=5&appid=bd3844093fc57703b2cc41751e23f944") #Currently Using Oroville as static API Call, change to [city] later
+    # city_code = 'Dallas'
+    # state_code = 'TX'
+    
+    # api_key = 
+    country_code = 'US'
+
+    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country_code}&limit=5&appid=bd3844093fc57703b2cc41751e23f944") #Currently Using Oroville as static API Call, change to [city] later
 
 
 
 
     if response.status_code == 200:
-        print("Response status was successful")
+        print("City Coordinates response status was successful")
         data = response.json()
 
         # Use latitude and longitude from "data" to make another API Call
-        # Example of a paramterized API Call that can contain weather data we want to extract: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+        # Example of a parameterized API Call that can contain weather data we want to extract: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
         # Example of a real API Call that can contain weather data we want to extract: https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
         if data:
             print("data if-statement was successful")
             print(data)
+            longitude = data[0]['lon']
+            latitude = data[0]['lat']
+            print("Now will print lat-long coordinates for Dallas,TX,US")
+            print("Dallas,TX,US Longitude:", longitude)
+            print("Dallas,TX,US Latitude:", latitude)
+
+            #Code is failing, I believe it is failing on the request line below
+            weather_response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat=32.77&lon=-96.79&exclude=minutely,hourly,daily,alerts&appid=bd3844093fc57703b2cc41751e23f944")
+
+            if weather_response.status_code == 200:
+                print("Weather_response status was successful")
+                weather_data = weather_response.json()
+                print(weather_data)
+            else:
+                print("Weather_response status failed with code:", weather_response.status_code) #Currently failing with Error 401, meaning unauthorized 
+                return None
+
             # print(f"Weather in London:")
             # print(f"Temperature: {data['main']['temp']} Fahrenheit")
         else:
