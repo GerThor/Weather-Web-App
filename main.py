f0e6fa44378ff48220c306a224a5fbeda91f95ec
Ger Thor <ger.thor97@gmail.com>
2024-10-07 18:17:18 -0700

    Added more work to main.py
    Added switch cases for user input of states into state codes. Added a bit of formatting for user input of cities.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template
-import requests
+import requests, math
 # Create a Flask application instance
 app = Flask(__name__)
 
@@ -16,8 +16,8 @@ def home():
 @app.route('/api/GetWeatherData', methods=['GET'])
 def get_data():
     # Extract city and state parameters from the request
-    city = request.args.get('city', 'unknown city')
-    state = request.args.get('state', 'unknown state')
+    city_input = request.args.get('city', 'unknown city')
+    state_input = request.args.get('state', 'unknown state')
 
     """
      TODO:  This method will call the method you create that makes the API call.
@@ -57,10 +57,121 @@ def get_data():
     # city_code = 'Dallas'
     # state_code = 'TX'
     
-    # api_key = 
-    country_code = 'US'
+    api_key = 'bd3844093fc57703b2cc41751e23f944'
 
-    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country_code}&limit=5&appid=bd3844093fc57703b2cc41751e23f944") #Currently Using Oroville as static API Call, change to [city] later
+    country_code = 'US'
+    
+
+    # still incomplete on city and state formatting, need to cover states with 2 or 3 words, not singular
+    # also need to make code into functions
+
+    city_input_formatted = city_input.title()
+
+    state_input_lower_case = state_input.lower()
+    match state_input_lower_case:
+        case "alabama":
+            state_code = "AL"
+        case "alaska":
+            state_code = "AK"
+        case "arizona":
+            state_code = "AZ"
+        case "arkansas":
+            state_code = "AR"
+        case "california":
+            state_code = "CA"
+        case "colorado":
+            state_code = "CO"
+        case "connecticut":
+            state_code = "CT"
+        case "delaware":
+            state_code = "DE"
+        case "florida":
+            state_code = "FL"
+        case "georgia":
+            state_code = "GA"
+        case "hawaii":
+            state_code = "HI"
+        case "idaho":
+            state_code = "ID"
+        case "illinois":
+            state_code = "IL"
+        case "indiana":
+            state_code = "IN"
+        case "iowa":
+            state_code = "IA"
+        case "kansas":
+            state_code = "KS"
+        case "kentucky":
+            state_code = "KY"
+        case "louisiana":
+            state_code = "LA"
+        case "maine":
+            state_code = "ME"
+        case "maryland":
+            state_code = "MD"
+        case "massachusetts":
+            state_code = "MA"
+        case "michigan":
+            state_code = "MI"
+        case "minnesota":
+            state_code = "MN"
+        case "mississippi":
+            state_code = "MS"
+        case "missouri":
+            state_code = "MO"
+        case "montana":
+            state_code = "MT"
+        case "nebraska":
+            state_code = "NE"
+        case "nevada":
+            state_code = "NV"
+        case "new hampshire":
+            state_code = "NH"
+        case "new jersey":
+            state_code = "NJ"
+        case "new mexico":
+            state_code = "NM"
+        case "new york":
+            state_code = "NY"
+        case "north carolina":
+            state_code = "NC"
+        case "north dakota":
+            state_code = "ND"
+        case "ohio":
+            state_code = "OH"
+        case "oklahoma":
+            state_code = "OK"
+        case "oregon":
+            state_code = "OR"
+        case "pennsylvania":
+            state_code = "PA"
+        case "rhode island":
+            state_code = "RI"
+        case "south carolina":
+            state_code = "SC"
+        case "south dakota":
+            state_code = "SD"
+        case "tennessee":
+            state_code = "TN"
+        case "texas":
+            state_code = "TX"
+        case "utah":
+            state_code = "UT"
+        case "vermont":
+            state_code = "VT"
+        case "virginia":
+            state_code = "VA"
+        case "washington":
+            state_code = "WA"
+        case "west virginia":
+            state_code = "WV"
+        case "wisconsin":
+            state_code = "WI"
+        case "wyoming":
+            state_code = "WY"
+        
+        
+    response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_input_formatted},{state_code},{country_code}&limit=5&appid={api_key}") #Currently Using Oroville as static API Call, change to [city] later
 
 
 
@@ -76,14 +187,14 @@ def get_data():
         if data:
             print("data if-statement was successful")
             print(data)
-            longitude = data[0]['lon']
-            latitude = data[0]['lat']
-            print("Now will print lat-long coordinates for Dallas,TX,US")
-            print("Dallas,TX,US Longitude:", longitude)
-            print("Dallas,TX,US Latitude:", latitude)
+            longitude = round(data[0]['lon'], 2)
+            latitude = round(data[0]['lat'], 2)
+            print("Now will print lat-long coordinates for:", city_input_formatted, state_input.title(), country_code)
+            print(city_input_formatted,", ", state_input.title(), ", ", country_code, ", ", "Longitude: ", longitude, sep='')
+            print(city_input_formatted,", ", state_input.title(),", ", country_code,", ", "Latitude: ", latitude, sep='')
 
             #Code is failing, I believe it is failing on the request line below
-            weather_response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat=32.77&lon=-96.79&exclude=minutely,hourly,daily,alerts&appid=bd3844093fc57703b2cc41751e23f944")
+            weather_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=imperial&appid={api_key}")
 
             if weather_response.status_code == 200:
                 print("Weather_response status was successful")
@@ -97,7 +208,7 @@ def get_data():
             # print(f"Temperature: {data['main']['temp']} Fahrenheit")
         else:
             print("Data retrieval failed")
-        return jsonify({"message": f"You sent: City - {city}, State - {state}"})
+        return jsonify({"message": f"You sent: City - {city_input.title()}, State - {state_input.title()}"})
     else:
         print("Response status failed.")
         return None
