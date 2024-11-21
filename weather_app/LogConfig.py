0c227e84b77e04fad914e9be79a66723f59cb8d9
Jack Thor <jack.sen.thor@gmail.com>
2024-11-21 00:26:02 -0800

    Logging
    Added Log config file, and updates open weathermap to start loggin

---
+++ weather_app/LogConfig.py
@@ -0,0 +1,34 @@
+import logging
+import logging.config
+
+# Best practice: use a configuration dictionary
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "level": "INFO",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "app.log",
+            "formatter": "default",
+            "level": "INFO",
+        },
+    },
+    "loggers": {
+        "": {  # root logger
+            "handlers": ["console", "file"],
+            "level": "INFO",
+        },
+    },
+}
+
+
